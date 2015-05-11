e427ae9205347814de26169504ca434866b16ea3
LIL717 <lhill7@me.com>
2015-05-11 16:36:41 -0700

    Initial Commit

---
+++ CoffeeKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeKit
+//
+//  Created by Lori Hill on 5/11/15.
+//  Copyright (c) 2015 Lori Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeKit/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CoffeeKit
+//
+//  Created by Lori Hill on 5/11/15.
+//  Copyright (c) 2015 Lori Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
